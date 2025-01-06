38b801021bbfd1e3ffa86d0c51ab93404f4c34cd
priyagithubuser <153211753+priyagithubuser@users.noreply.github.com>
2025-01-06 21:00:10 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_6thnnOaK7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '10nlujv6pt2qed8j8i8hgtf3in', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
